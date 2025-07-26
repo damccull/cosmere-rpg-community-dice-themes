433d43e4b9175e16c28e07d1a073dd9949542b06
damccull <401104+damccull@users.noreply.github.com>
2025-07-25 17:20:17 -1000

    Add the rest of the dice themes

--- cosmere-dice.js
+++ cosmere-dice.js
@@ -23,8 +23,20 @@ const DSN_MODE = "default";
 
 // Define theme IDs to prevent hard coded ID names if they need changed
 const THEME_BASIC_STORMLIGHT = "basic_stormlight";
-const THEME_WINDRUNNER = "windrunner";
 const THEME_ELSECALLER = "elsecaller";
+const THEME_TRUTHWATCHER = "truthwatcher";
+const THEME_SKYBREAKER = "skybreaker";
+const THEME_LIGHTWEAVER = "lightweaver";
+const THEME_DUSTBRINGER = "dustbringer";
+const THEME_WILLSHAPER = "willshaper";
+const THEME_WINDRUNNER = "windrunner";
+const THEME_EDGEDANCER = "edgedancer";
+const THEME_STONEWARD = "stoneward";
+const THEME_BONDSMITH = "bondsmith";
+const THEME_VOIDLIGHT = "voidlight";
+const THEME_LIFELIGHT = "lifelight";
+const THEME_TOWERLIGHT = "towerlight";
+const THEME_WARLIGHT = "warlight";
 
 // Define themes
 const THEMES = {
@@ -62,6 +74,91 @@ const THEMES = {
       visibility: 'visible'
     },
   },
+  [THEME_TRUTHWATCHER]: {
+    description: 'Truthwatcher',
+    system: THEME_TRUTHWATCHER,
+    colorset: {
+      name: THEME_TRUTHWATCHER,
+      description: 'Truthwatcher',
+      category: DSN_GROUP,
+      foreground: '#efcd25',
+      background: '#2E684A',
+      outline: '#2E684A',
+      edge: '#2E684A',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_SKYBREAKER]: {
+    description: 'Skybreaker',
+    system: THEME_SKYBREAKER,
+    colorset: {
+      name: THEME_SKYBREAKER,
+      description: 'Skybreaker',
+      category: DSN_GROUP,
+      foreground: '#fafafa',
+      background: '#555564',
+      outline: '#555564',
+      edge: '#555564',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_LIGHTWEAVER]: {
+    description: 'Lightweaver',
+    system: THEME_LIGHTWEAVER,
+    colorset: {
+      name: THEME_LIGHTWEAVER,
+      description: 'Lightweaver',
+      category: DSN_GROUP,
+      foreground: '#fafafa',
+      background: '#641022',
+      outline: '#641022',
+      edge: '#641022',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_DUSTBRINGER]: {
+    description: 'Dustbringer',
+    system: THEME_DUSTBRINGER,
+    colorset: {
+      name: THEME_DUSTBRINGER,
+      description: 'Dustbringer',
+      category: DSN_GROUP,
+      foreground: '#fdb73f',
+      background: '#B22330',
+      outline: '#B22330',
+      edge: '#B22330',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_WILLSHAPER]: {
+    description: 'Willshaper',
+    system: THEME_WILLSHAPER,
+    colorset: {
+      name: THEME_WILLSHAPER,
+      description: 'Willshaper',
+      category: DSN_GROUP,
+      foreground: '#fdb73f',
+      background: '#601F64',
+      outline: '#601F64',
+      edge: '#601F64',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
   [THEME_WINDRUNNER]: {
     description: 'Windrunner',
     system: THEME_WINDRUNNER,
@@ -78,7 +175,126 @@ const THEMES = {
       font: 'Arial',
       visibility: 'visible'
     },
-  }
+  },
+  [THEME_EDGEDANCER]: {
+    description: 'Edgedancer',
+    system: THEME_EDGEDANCER,
+    colorset: {
+      name: THEME_EDGEDANCER,
+      description: 'Edgedancer',
+      category: DSN_GROUP,
+      foreground: '#555564',
+      background: '#C8CFE6',
+      outline: '#C8CFE6',
+      edge: '#C8CFE6',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_STONEWARD]: {
+    description: 'Stoneward',
+    system: THEME_STONEWARD,
+    colorset: {
+      name: THEME_STONEWARD,
+      description: 'Stoneward',
+      category: DSN_GROUP,
+      foreground: '#d53815',
+      background: '#d79447',
+      outline: '#d79447',
+      edge: '#d79447',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_BONDSMITH]: {
+    description: 'Bondsmith',
+    system: THEME_BONDSMITH,
+    colorset: {
+      name: THEME_BONDSMITH,
+      description: 'Bondsmith',
+      category: DSN_GROUP,
+      foreground: '#d57c15',
+      background: '#E7C852',
+      outline: '#533a1d',
+      edge: '#E7C852',
+      texture: 'ice_2',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_VOIDLIGHT]: {
+    description: 'Voidlight',
+    system: THEME_VOIDLIGHT,
+    colorset: {
+      name: THEME_VOIDLIGHT,
+      description: 'Voidlight',
+      category: DSN_GROUP,
+      foreground: '#9d3fcf',
+      background: '#601F64',
+      outline: '#601F64',
+      edge: '#601F64',
+      texture: 'fire',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_LIFELIGHT]: {
+    description: 'Lifelight',
+    system: THEME_LIFELIGHT,
+    colorset: {
+      name: THEME_LIFELIGHT,
+      description: 'Lifelight',
+      category: DSN_GROUP,
+      foreground: '#dcffdb',
+      background: '#40f76e',
+      outline: '#6dfd77',
+      edge: '#6dfd77',
+      texture: 'astral',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_TOWERLIGHT]: {
+    description: 'Towerlight',
+    system: THEME_TOWERLIGHT,
+    colorset: {
+      name: THEME_TOWERLIGHT,
+      description: 'Towerlight',
+      category: DSN_GROUP,
+      foreground: '#37ae77',
+      background: '#dce5f4',
+      outline: '#3e6ac1',
+      edge: '#c4d6f8',
+      texture: 'bronze03a',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
+  [THEME_WARLIGHT]: {
+    description: 'Warlight',
+    system: THEME_WARLIGHT,
+    colorset: {
+      name: THEME_WARLIGHT,
+      description: 'Warlight',
+      category: DSN_GROUP,
+      foreground: '#3357e6',
+      background: '#1f096c',
+      outline: '#000000',
+      edge: '#000000',
+      texture: 'marble',
+      material: 'glass',
+      font: 'Arial',
+      visibility: 'visible'
+    },
+  },
 }
 
 /// Generates numerical dice
